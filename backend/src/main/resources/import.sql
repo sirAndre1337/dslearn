a66536e1405c3cd334bfbba8c7bfbbbbe0a59196
André Luis <andrelluis17@hotmail.com>
2022-02-22 16:32:05 -0300

    Resource

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -21,3 +21,7 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Primeiro feedback de tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-10T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Segundo feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-12T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Terceiro feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-12-14T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type , offer_id) VALUES ('Trilha HTML', 'trilha principal do curso' , 1 , 'https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2021/06/Curso-de-programa%C3%A7%C3%A3o.jpg', 1 , 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type , offer_id) VALUES ('Forum', 'Tire suas duvidas' , 2 , 'https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2021/06/Curso-de-programa%C3%A7%C3%A3o.jpg', 2 , 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type , offer_id) VALUES ('Lives', 'lives exclusivas para a turma' , 3 , 'https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2021/06/Curso-de-programa%C3%A7%C3%A3o.jpg', 0 , 1);
